faac26bf970c8177083356159ea4abd30683721a
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-11-07 10:03:18 +0300

    Validations

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,6 +3,7 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from "vee-validate";
 import {
   required,
@@ -23,13 +24,38 @@ export default {
     app.component("ErrorMessage", ErrorMessage);
 
     defineRule("required", required);
+    defineRule("tos", required);
     defineRule("min", min);
     defineRule("max", max);
     defineRule("alpha_spaces", alphaSpaces);
     defineRule("email", email);
     defineRule("min_value", minVal);
     defineRule("max_value", maxVal);
-    defineRule("confirmed", confirmed);
+    defineRule("password_mismatch", confirmed);
     defineRule("excluded", excluded);
+    defineRule("country_excluded", excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `This field ${ctx.field} is required`,
+          min: `This field ${ctx.field} is too short`,
+          max: `This field ${ctx.field} is too long`,
+          alpa_spaces: `This field ${ctx.field} must contain only letters and spaces`,
+          email: `This field ${ctx.field} must be a valid email`,
+          min_value: `This field ${ctx.field} is too low`,
+          max_value: `This field ${ctx.field} is too high`,
+          excluded: `This field ${ctx.field} is not allowed`,
+          country_excluded:
+            "Due to restrictions, we don't accept users form this location.",
+          pasword_mismatch: "The passwords do not match",
+          tos: "You must agree to the terms of service",
+        };
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid`;
+      },
+    });
   },
 };
